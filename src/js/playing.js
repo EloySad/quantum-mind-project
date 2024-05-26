8565babc028804a0255372d04afd13765ded7b5b
cilantech <isazaherbin@gmail.com>
2024-05-26 12:32:08 -0500

    feat: Se crea nueva vista modelo para juegos

--- src/js/playing.js
+++ src/js/playing.js
@@ -3,3 +3,12 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+// Abre el modal
+function abrirModal() {
+    document.getElementById("miModal").style.display = "block";
+  }
+  
+  // Asigna el evento al botón
+  document.getElementById("abrirModal").addEventListener("click", abrirModal);
+  
