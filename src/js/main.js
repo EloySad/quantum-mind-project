883e8ae9d32d6296a0d7312fbcf825d71100d737
AlejandroLedesma54 <davidnaples2020@gmail.com>
2024-05-28 23:58:54 -0500

    feat: we changed header, footer and card with bootstrap.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,24 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
+
+let lastScrollTop = 0;
+let headerHeight = document.querySelector("header").offsetHeight;
+window.addEventListener(
+  "scroll",
+  function () {
+    let currentScroll =
+      window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop && currentScroll > headerHeight) {
+      // Desplazamiento hacia abajo
+      document.querySelector("header").style.top = "-" + headerHeight + "px";
+    } else {
+      // Desplazamiento hacia arriba o en la parte superior de la página
+      document.querySelector("header").style.top = "0";
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  },
+  false
+);
